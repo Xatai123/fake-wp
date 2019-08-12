5995b98cd154c67a097ca324abb2e7ec5f580daa
Xatai123 <xataialiyev5@gmail.com>
2019-08-13 01:24:50 +0400

    refactored code to be more workable

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
 //Show Time on Top Left Corner
 function showTime() {
-
     let time = new Date();
 
     let timeString = time.getHours().format() + ":" + time.getMinutes().format();
-    document.getElementsByClassName("timeDisplay")[0].innerHTML = timeString;
-    document.getElementsByClassName("timeDisplay")[1].innerHTML = timeString;
+    let clocks = document.getElementsByClassName("timeDisplay");
+    clocks[0].innerHTML = timeString;
+    clocks[1].innerHTML = timeString;
 
 
 }
@@ -24,17 +24,59 @@ function send(i) {
     if (text === "")
         return;
     document.getElementsByTagName("input")[i].value = "";
-    let owner = '<li onclick="deleteForEveryone(this)">Delete for everyone</li>';
-    let sendHtml1 = text
-        + '</p> <button class="delete" onclick="deleteButton(this)"> <i class="fas fa-chevron-down"></i> <ul> <li onclick="deleteForMe(this)">Delete for me</li> ';
-    let sendHtml2 = '</ul> </button><h5>'
-        + timeString
-        + ' <i class="fas fa-check"> </i> </h5></div>';
 
-    document.getElementsByClassName("chat")[i].innerHTML += ' <div class="message"> <p> ' + sendHtml1 + owner + sendHtml2;
+    document.getElementsByClassName("chat")[i].appendChild(construct(true, text, timeString))
     k = i === 0 ? 1 : 0;
-    document.getElementsByClassName("chat")[k].innerHTML += ' <div class="response"> <p> ' + sendHtml1 + sendHtml2;
+    document.getElementsByClassName("chat")[k].appendChild(construct(false, text, timeString))
+
     updateScroll();
+}
+
+
+// constructs new message to send
+function construct(boo, text, time) {
+
+    let div = document.createElement("div");
+
+    if (boo) {
+        div.classList.add("message");
+    } else {
+        div.classList.add("response");
+    }
+
+    let p = document.createElement("p");
+    p.innerText = text;
+    div.appendChild(p);
+
+    let button = document.createElement("button");
+    button.classList.add("delete");
+    button.setAttribute("onclick", "deleteButton(this)");
+
+    button.innerHTML += '<i class="fas fa-chevron-down"></i>';
+
+    let ul = document.createElement("ul");
+
+    let li1 = document.createElement("li");
+    li1.innerText = "Delete for me";
+    li1.setAttribute("onclick", "deleteForMe(this)");
+    ul.appendChild(li1);
+
+    if (boo) {
+        let li2 = document.createElement("li");
+        li2.innerText = "Delete for everyone";
+        li2.setAttribute("onclick", "deleteForEveryone(this)");
+        ul.appendChild(li2);
+    }
+
+    button.appendChild(ul);
+
+    div.appendChild(button);
+
+    h5 = document.createElement("h5");
+    h5.innerHTML = time + ' <i class="fas fa-check"></i>';
+    div.appendChild(h5);
+
+    return div;
 }
 
 // Send with Enter 
@@ -44,7 +86,6 @@ for (let i = 0; i < 2; i++) {
             send(i);
         }
     });
-
 }
 
 // Scroll Bottom to See Latest Messages
@@ -58,27 +99,30 @@ function updateScroll() {
 
 // Delete Message
 function deleteButton(e) {
-    e.childNodes[3].classList.toggle("d-block");
+    e.childNodes[1].classList.toggle("d-block");
 }
 
 function deleteForMe(e) {
-    var elem = e.parentElement.parentElement.parentElement;
+    let elem = e.parentElement.parentElement.parentElement;
     elem.classList.toggle("d-none");
 }
 
 function deleteForEveryone(e) {
-    e.parentElement.classList.toggle("d-block");
-    var elem = e.parentElement.parentElement.parentElement;
+    let elem = e.parentElement.parentElement.parentElement;
     let index = elem.parentElement.childNodes.indexOf(elem);
     let chat = document.getElementsByClassName("chat");
     for (let i = 0; i < chat.length; i++) {
-        let k = chat[i].childNodes[index].innerHTML;
-        chat[i].childNodes[index].innerHTML = k.substr(0, k.indexOf("<p>") + 3) + "<strong style='color: grey'>This message is deleted</strong>" + k.substr(k.indexOf("<", k.indexOf("</p>")));
-        let removed = chat[i].childNodes[index].childNodes[3].childNodes[3].childNodes[3];
+        let k = chat[i].childNodes[index];
+        k.removeChild(k.firstChild);
+        let p = document.createElement("p");
+        p.innerHTML = "<strong style='color: grey'>This message is deleted</strong>";
+        k.insertBefore(p, k.firstChild)
+        let removed = k.childNodes[1].childNodes[1].childNodes[1];
         if (removed) {
-            removed.parentElement.removeChild(removed);
+            removed.parentElement.removeChild(removed);            
         }
     }
+    
 }
 
 NodeList.prototype.indexOf = function (item) {
